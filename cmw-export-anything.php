19a136f497e49d3afcdadfa7947220bc945735af
Sanchit <ansifers@gmail.com>
2025-02-09 17:10:52 +0530

    Change Version

--- cmw-export-anything.php
+++ cmw-export-anything.php
@@ -4,7 +4,7 @@
  * Description: This plugin allows you to export anything from your WordPress site.
  * Author: CodeMyWP
  * Author uri: https://www.codemywp.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.2
  * Requires PHP: 7.4
  * Tested up to: 6.7
